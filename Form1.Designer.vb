a4ec10e58a1a5a8fa295bc712ea028325f905d14
daben26 <90689408+daben26@users.noreply.github.com>
2022-07-01 21:14:14 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.lblAnswer = New System.Windows.Forms.Label
+        Me.txt2 = New System.Windows.Forms.TextBox
+        Me.txt1 = New System.Windows.Forms.TextBox
+        Me.cmdadd = New System.Windows.Forms.Button
+        Me.cmdmultiply = New System.Windows.Forms.Button
+        Me.cmdsubtract = New System.Windows.Forms.Button
+        Me.cmddivide = New System.Windows.Forms.Button
+        Me.cmdexit = New System.Windows.Forms.Button
+        Me.cmdclear = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(92, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Integers  "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(15, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "First Integer"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 18)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Second Integer"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(23, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 18)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Answer"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAnswer.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(131, 122)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(100, 23)
+        Me.lblAnswer.TabIndex = 4
+        '
+        'txt2
+        '
+        Me.txt2.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt2.Location = New System.Drawing.Point(131, 85)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(100, 25)
+        Me.txt2.TabIndex = 5
+        '
+        'txt1
+        '
+        Me.txt1.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt1.Location = New System.Drawing.Point(131, 51)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(100, 25)
+        Me.txt1.TabIndex = 6
+        '
+        'cmdadd
+        '
+        Me.cmdadd.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdadd.Location = New System.Drawing.Point(12, 163)
+        Me.cmdadd.Name = "cmdadd"
+        Me.cmdadd.Size = New System.Drawing.Size(113, 23)
+        Me.cmdadd.TabIndex = 7
+        Me.cmdadd.Text = "ADD"
+        Me.cmdadd.UseVisualStyleBackColor = True
+        '
+        'cmdmultiply
+        '
+        Me.cmdmultiply.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdmultiply.Location = New System.Drawing.Point(12, 192)
+        Me.cmdmultiply.Name = "cmdmultiply"
+        Me.cmdmultiply.Size = New System.Drawing.Size(113, 23)
+        Me.cmdmultiply.TabIndex = 8
+        Me.cmdmultiply.Text = "MULTIPLY"
+        Me.cmdmultiply.UseVisualStyleBackColor = True
+        '
+        'cmdsubtract
+        '
+        Me.cmdsubtract.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdsubtract.Location = New System.Drawing.Point(164, 163)
+        Me.cmdsubtract.Name = "cmdsubtract"
+        Me.cmdsubtract.Size = New System.Drawing.Size(115, 23)
+        Me.cmdsubtract.TabIndex = 9
+        Me.cmdsubtract.Text = "SUBTRACT"
+        Me.cmdsubtract.UseVisualStyleBackColor = True
+        '
+        'cmddivide
+        '
+        Me.cmddivide.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmddivide.Location = New System.Drawing.Point(164, 197)
+        Me.cmddivide.Name = "cmddivide"
+        Me.cmddivide.Size = New System.Drawing.Size(115, 23)
+        Me.cmddivide.TabIndex = 10
+        Me.cmddivide.Text = "DIVIDE"
+        Me.cmddivide.UseVisualStyleBackColor = True
+        '
+        'cmdexit
+        '
+        Me.cmdexit.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdexit.Location = New System.Drawing.Point(144, 226)
+        Me.cmdexit.Name = "cmdexit"
+        Me.cmdexit.Size = New System.Drawing.Size(87, 23)
+        Me.cmdexit.TabIndex = 11
+        Me.cmdexit.Text = "EXIT"
+        Me.cmdexit.UseVisualStyleBackColor = True
+        '
+        'cmdclear
+        '
+        Me.cmdclear.Font = New System.Drawing.Font("Garamond", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdclear.Location = New System.Drawing.Point(57, 226)
+        Me.cmdclear.Name = "cmdclear"
+        Me.cmdclear.Size = New System.Drawing.Size(81, 23)
+        Me.cmdclear.TabIndex = 12
+        Me.cmdclear.Text = "CLEAR"
+        Me.cmdclear.UseVisualStyleBackColor = True
+        '
+        'frmCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 282)
+        Me.Controls.Add(Me.cmdclear)
+        Me.Controls.Add(Me.cmdexit)
+        Me.Controls.Add(Me.cmddivide)
+        Me.Controls.Add(Me.cmdsubtract)
+        Me.Controls.Add(Me.cmdmultiply)
+        Me.Controls.Add(Me.cmdadd)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmCalc"
+        Me.Text = "First Application"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents txt2 As System.Windows.Forms.TextBox
+    Friend WithEvents txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents cmdadd As System.Windows.Forms.Button
+    Friend WithEvents cmdmultiply As System.Windows.Forms.Button
+    Friend WithEvents cmdsubtract As System.Windows.Forms.Button
+    Friend WithEvents cmddivide As System.Windows.Forms.Button
+    Friend WithEvents cmdexit As System.Windows.Forms.Button
+    Friend WithEvents cmdclear As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class frmCalc
+
+    Private Sub cmdadd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdadd.Click
+        Dim integer1 As Integer
+        Dim integer2 As Integer
+        Dim answer As Integer
+        integer1 = Val(txt1.Text)
+        integer2 = Val(txt2.Text)
+        answer = integer1 + integer2
+        lblAnswer.Text = answer
+
+
+    End Sub
+
+    Private Sub cmdsubtract_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdsubtract.Click
+        Dim integer1 As Integer
+        Dim integer2 As Integer
+        Dim answer As Integer
+        integer1 = Val(txt1.Text)
+        integer2 = Val(txt2.Text)
+        answer = integer1 - integer2
+        lblAnswer.Text = answer
+    End Sub
+
+    Private Sub cmdmultiply_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdmultiply.Click
+        Dim integer1 As Integer
+        Dim integer2 As Integer
+        Dim answer As Integer
+        integer1 = Val(txt1.Text)
+        integer2 = Val(txt2.Text)
+        answer = integer1 * integer2
+        lblAnswer.Text = answer
+    End Sub
+
+    Private Sub cmddivide_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmddivide.Click
+        Dim integer1 As Integer
+        Dim integer2 As Integer
+        Dim answer As Integer
+        integer1 = Val(txt1.Text)
+        integer2 = Val(txt2.Text)
+        answer = integer1 / integer2
+        lblAnswer.Text = answer
+    End Sub
+
+    Private Sub cmdclear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdclear.Click
+        txt1.Text = ""
+        txt2.Text = ""
+        lblAnswer.Text = ""
+        txt1.Focus()
+    End Sub
+
+    Private Sub cmdexit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdexit.Click
+        Me.Close()
+    End Sub
+End Class
